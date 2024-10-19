fb053efb327bfa11cecd098bb74ce2819bf0f826
Md Milon <abireshan32@gmail.com>
2024-10-19 16:26:46 +0600

    add nodemailer

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const app=express()
-const port=(process.env.PORT||3000)
+const app = express()
+const port = (process.env.PORT || 3000)
+const { ObjectId } = require('mongodb');
+const nodemailer=require('nodemailer')
+//middleware
+app.use(cors({
+ origin:['http://localhost:5173'] 
+}))
+app.use(express.json())
 
- //middleware
+//setup transporter
+const transporter=nodemailer.createTransport({
+  service:"gmail",
+  auth:{
+    user:process.env.MAIL,
+    pass:process.env.MAIL_PSS
+  }
+})
 
- app.use(cors())
- app.use(express.json())
-console.log(process.env.DB_PASS);
+//mongoDB connection
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pnsxsk9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,33 +32,64 @@ const client = new MongoClient(uri, {
   }
 });
 
-const doctorCollection=client.db('doctor-house').collection('doctors')
-const appointmentCollection=client.db('doctor-house').collection('appointment')
+const doctorCollection = client.db('doctor-house').collection('doctors')
+const appointmentCollection = client.db('doctor-house').collection('appointment')
 
 async function run() {
   try {
 
-    app.get('/',async(req,res)=>{
-        res.send('Doctor server running')
+    app.get('/', async (req, res) => {
+      res.send('Doctor server running')
     })
 
-    app.get('/doctor',async(req,res)=>{
-        const result=await doctorCollection.find().toArray()
-         res.send(result)
+    app.get('/doctor', async (req, res) => {
+      const result = await doctorCollection.find().toArray()
+      res.send(result)
     })
-
-    app.post('/appointment',async(req,res)=>{
-      const data=req.body
+    app.get('/doctor/:id', async (req, res) => {
+      try {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }; 
+          console.log(query);
+       
+          const result = await doctorCollection.findOne(query);
+          if (result) {
+              res.send(result);
+          } else {
+              res.status(404).send({ message: 'Doctor not found' });
+          }
+      } catch (error) {
+          console.error(error);
+          res.status(500).send({ message: 'Error fetching doctor details' });
+      }
+  });
+    app.post('/appointment', async (req, res) => {
+      const data = req.body
       console.log(data);
+      const result = await appointmentCollection.insertOne(data)
     })
-    await client.db("admin").command({ ping: 1 });
+
+    app.post('/send_mail',async(req,res)=>{
+      const {email,name,doctorName}=req.body
+      console.log(data);
+      const mailOption={
+        from:email,
+        to:process.env.MAIL,
+        subject:`Contact By ${name}`,
+        text:`you have new massage from
+        Name:${name}
+        Email:${email}
+        Message:I need doctor ${doctorName}
+        `
+      }
+    })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
   }
 }
 run().catch(console.dir);
 
-app.listen(port,()=>{
-    console.log(`doctor house runing at ${port}`);
+app.listen(port, () => {
+  console.log(`doctor house runing at ${port}`);
 })
